8bbd2eb4c8c62669eabd7bbaa113d5ea73fe4aae
Abhay-wdev <abhaychorsiya.dev@gmail.com>
2025-11-21 13:57:19 +0530

    ok

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,9 +1,13 @@
+import Blog from "../components/Blog";
 import ProductsPage from "../components/Productspage";
+import ReviewSection from "../components/ReviewSection";
 
  export default function Products() {
    return (
     <>
    <ProductsPage/>
+   <ReviewSection/>
+   <Blog/>
     </>
    );
  }
